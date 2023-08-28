10d04c6bd66bc48e594833973b6bf16cb530cc6b
renzo971 <orrego.st.santos@gmail.com>
2023-08-28 13:50:12 -0500

    filtros de invitados

---
+++ script-details.js
@@ -0,0 +1,29 @@
+// Obtener el parámetro del nombre del invitado de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const invitadoNombre = urlParams.get('invitado');
+
+// Cargar el archivo JSON de invitados
+fetch('archivo.json')
+  .then(response => response.json())
+  .then(data => {
+    const detallesContainer = document.getElementById('detalles');
+    const invitado = data.invitados.find(i => i.nombre === invitadoNombre);
+
+    if (invitado) {
+      const detallesDiv = document.createElement('div');
+      detallesDiv.className = 'invitado-details-card';
+      detallesDiv.innerHTML = `
+      <p>Es un placer que puedas acompañarnos en este dia tan especial para nosotros</p>
+      <h4>Invitada</h4>
+        <h3>${invitado.nombre}</h3>
+        <p>Acompañantes: ${invitado.acompañantes}</p>
+      `;
+      detallesContainer.appendChild(detallesDiv);
+    } else {
+      const errorDiv = document.createElement('div');
+      errorDiv.className = 'error-message';
+      errorDiv.textContent = 'Invitado no encontrado.';
+      detallesContainer.appendChild(errorDiv);
+    }
+  })
+  .catch(error => console.error('Error al cargar los datos:', error));
--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+const searchInput = document.getElementById('searchInput');
+const suggestionsContainer = document.getElementById('suggestions');
+const invitadosContainer = document.getElementById('invitados');
+
+// Cargar el archivo JSON de invitados
 fetch('archivo.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
-    const invitadosContainer = document.getElementById('invitados');
-    data.invitados.forEach(invitado => {
+    const invitados = data.invitados;
+
+    // Función para mostrar sugerencias en tiempo real
+// Función para mostrar sugerencias en tiempo real
+function showSuggestions(searchTerm) {
+  suggestionsContainer.innerHTML = '';
+
+  if (searchTerm === '') {
+    displayInvitadoDetails(invitados)
+    return; // No mostrar sugerencias si el campo está vacío
+  }
+
+  const filteredInvitados = invitados.filter(
+    invitado => invitado.nombre.toLowerCase().includes(searchTerm)
+  );
+
+  const ul = document.createElement('ul');
+  ul.className = 'list-group';
+
+  filteredInvitados.forEach(invitado => {
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.textContent = invitado.nombre;
+    li.addEventListener('click', () => {
+      searchInput.value = invitado.nombre;
+      suggestionsContainer.innerHTML = '';
+      displayInvitadoDetails(invitado);
+    });
+    ul.appendChild(li);
+  });
+
+  suggestionsContainer.appendChild(ul);
+}
+
+
+
+    // Función para mostrar los detalles del invitado
+    function displayInvitadoDetails(invitado) {
       const invitadoDiv = document.createElement('div');
       invitadoDiv.className = 'invitado-card';
       invitadoDiv.innerHTML = `
@@ -12,6 +52,18 @@ fetch('archivo.json')
         <a href="home.html?invitado=${encodeURIComponent(invitado.nombre)}">Ver invitacion</a>
       `;
       invitadosContainer.appendChild(invitadoDiv);
+    }
+
+    // Manejar la entrada del usuario en tiempo real
+    searchInput.addEventListener('input', () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      if (searchTerm.length >= 2) {
+        showSuggestions(searchTerm);
+      } else {
+        suggestionsContainer.innerHTML = '';
+      }
     });
+
+    // ... Resto del código ...
   })
   .catch(error => console.error('Error al cargar los datos:', error));
